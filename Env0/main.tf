fe419da13f9b500a1b967565c01bcb700fbc99ea
baderholdt <baderholdt@alarm.com>
2023-05-09 09:09:33 -0400

    terrakube and env0

---
+++ Env0/main.tf
@@ -0,0 +1,49 @@
+##########################################################################################
+################################# PROVIDER DEFINITION ####################################
+##########################################################################################
+
+terraform {
+  required_version = ">= 1.1.6"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.53.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+  }
+
+  client_id       = var.azurerm_client_id
+  client_secret   = var.azurerm_client_secret
+  tenant_id       = var.azurerm_tenant_id
+  subscription_id = var.azurerm_subscription_id
+}
+
+##########################################################################################
+##################################### RESOURCES ##########################################
+##########################################################################################
+
+module "example_azure_resource" {
+  source = "api.env0.com/242cfbe5-3a35-40b9-b5f9-5e67c4c4b348/TerraformTestModule/azurerm"
+  version = "1.0.4"
+
+  resource_group_name            = var.resource_group_name
+  resource_group_location        = var.resource_group_location
+
+  virtual_network_name           = var.virtual_network_name
+
+  network_interface_name         = var.network_interface_name
+
+  virtual_machine_name           = var.virtual_machine_name
+  virtual_machine_size           = var.virtual_machine_size
+  virtual_machine_iso_publisher  = var.virtual_machine_iso_publisher
+  virtual_machine_iso_offer      = var.virtual_machine_iso_offer
+  virtual_machine_iso_sku        = var.virtual_machine_iso_sku
+  virtual_machine_iso_version    = var.virtual_machine_iso_version
+
+  virtual_machine_admin_username = var.virtual_machine_admin_username
+  virtual_machine_admin_password = var.virtual_machine_admin_password
+}
---
+++ Terrakube/terraform.tfvars
@@ -0,0 +1,13 @@
+resource_group_name            = "AZURE.ALARM.DEV.NA03.TERRAKUBE_TEST.NET.INTERNAL"
+resource_group_location        = "USEAST"
+
+virtual_network_name           = "terrakube_test_network"
+
+network_interface_name         = "terrakube_test_network_interface"
+
+virtual_machine_name           = "na03ltestd03"
+virtual_machine_size           = "Standard_F2"
+virtual_machine_iso_publisher  = "Canonical"
+virtual_machine_iso_offer      = "UbuntuServer"
+virtual_machine_iso_sku        = "16.04-LTS"
+virtual_machine_iso_version    = "latest"
